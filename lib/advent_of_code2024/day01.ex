5eea41b787c3f2d87f3647c393d86e6df9015bca
Brad Feehan <git@bradfeehan.com>
2024-12-01 22:04:13 +1100

    Complete day 1 part 2

--- lib/advent_of_code2024/day01.ex
+++ lib/advent_of_code2024/day01.ex
@@ -40,9 +40,39 @@ defmodule AdventOfCode2024.Day01 do
 
   @doc """
   Solves part 2 of the day's challenge
+
+  ## Examples
+
+      iex> input = \"""
+      ...> 3   4
+      ...> 4   3
+      ...> 2   5
+      ...> 1   3
+      ...> 3   9
+      ...> 3   3
+      ...> \"""
+      iex> AdventOfCode2024.Day01.part2(input)
+      31
+
   """
   def part2(input) do
-    # We'll implement this once part 2 is revealed
-    input
+    {left_list, right_list} =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [left, right] = String.split(line, ~r/\s+/, trim: true)
+        {String.to_integer(left), String.to_integer(right)}
+      end)
+      |> Enum.unzip()
+
+    # Convert right list to a frequency map
+    right_frequencies = Enum.frequencies(right_list)
+
+    # For each number in left list, multiply by its frequency in right list (or 0 if not found)
+    left_list
+    |> Enum.map(fn num ->
+      num * Map.get(right_frequencies, num, 0)
+    end)
+    |> Enum.sum()
   end
 end
--- test/advent_of_code2024/day01_test.exs
+++ test/advent_of_code2024/day01_test.exs
@@ -1,68 +1,62 @@
 defmodule AdventOfCode2024.Day01Test do
   use ExUnit.Case
-  alias AdventOfCode2024.Day01
   doctest AdventOfCode2024.Day01
 
+  alias AdventOfCode2024.Day01
+
+  @example_input """
+  3   4
+  4   3
+  2   5
+  1   3
+  3   9
+  3   3
+  """
+
   describe "part1/1" do
-    test "handles single pair" do
-      input = "1   5\n"
-      assert Day01.part1(input) == 4
-    end
-
-    test "handles identical numbers in different orders" do
-      input = """
-      3   7
-      7   1
-      1   3
-      """
-
-      assert Day01.part1(input) == 0
-    end
-
-    test "handles negative differences" do
-      input = """
-      7   2
-      10  4
-      15  8
-      """
-
-      # (5 + 6 + 7)
-      assert Day01.part1(input) == 18
-    end
-
-    test "handles negative numbers" do
-      input = """
-      -3   5
-      -7   -2
-      4    -6
-      """
-
-      assert Day01.part1(input) == 3
+    test "solves the example case" do
+      assert Day01.part1(@example_input) == 11
     end
 
     test "handles empty input" do
       assert Day01.part1("") == 0
     end
 
-    test "handles malformed input" do
-      assert_raise ArgumentError, fn ->
-        Day01.part1("not-a-number  5\n")
-      end
-
-      assert_raise MatchError, fn ->
-        Day01.part1("1  2  3\n")
-      end
-
-      assert_raise MatchError, fn ->
-        Day01.part1("1\n")
-      end
+    test "handles single line" do
+      assert Day01.part1("5   8") == 3
     end
   end
 
   describe "part2/1" do
-    test "placeholder - doesn't raise" do
-      input = "1   2\n"
-      Day01.part2(input)
+    test "solves the example case" do
+      assert Day01.part2(@example_input) == 31
+    end
+
+    test "handles empty input" do
+      assert Day01.part2("") == 0
+    end
+
+    test "handles single line" do
+      assert Day01.part2("5   5") == 5
+    end
+
+    test "handles case with no matches" do
+      input = """
+      1   2
+      3   4
+      5   6
+      """
+
+      assert Day01.part2(input) == 0
+    end
+
+    test "handles case with all matches" do
+      input = """
+      2   2
+      2   2
+      """
+
+      assert Day01.part2(input) == 8
     end
   end
 end
