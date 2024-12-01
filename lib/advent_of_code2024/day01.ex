6146638a8a66415447623e14afcf4599d685653a
Brad Feehan <git@bradfeehan.com>
2024-12-01 20:01:51 +1100

    Initial day 1 solution

---
+++ lib/advent_of_code2024/day01.ex
@@ -0,0 +1,34 @@
+defmodule AdventOfCode2024.Day01 do
+  @moduledoc """
+  Solution for Advent of Code 2024, Day 1
+  """
+
+  @doc """
+  Solves part 1 of the day's challenge
+  """
+  def part1(input) do
+    {left_list, right_list} =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [left, right] = String.split(line, ~r/\s+/, trim: true)
+        {String.to_integer(left), String.to_integer(right)}
+      end)
+      |> Enum.unzip()
+
+    left_sorted = Enum.sort(left_list)
+    right_sorted = Enum.sort(right_list)
+
+    Enum.zip(left_sorted, right_sorted)
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.sum()
+  end
+
+  @doc """
+  Solves part 2 of the day's challenge
+  """
+  def part2(input) do
+    # We'll implement this once part 2 is revealed
+    input
+  end
+end
---
+++ test/advent_of_code2024/day01_test.exs
@@ -0,0 +1,22 @@
+defmodule AdventOfCode2024.Day01Test do
+  use ExUnit.Case
+  alias AdventOfCode2024.Day01
+
+  describe "part1/1" do
+    test "solves the example" do
+      input = """
+      """
+
+      assert Day01.part1(input) == 0
+    end
+  end
+
+  describe "part2/1" do
+    test "solves the example" do
+      input = """
+      """
+
+      assert Day01.part2(input) == 0
+    end
+  end
+end
