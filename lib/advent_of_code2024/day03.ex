b187e7af8ee01972385757fab2c0a1337ef6f772
Brad Feehan <git@bradfeehan.com>
2024-12-08 00:30:15 +1100

    day 3 part 2

--- lib/advent_of_code2024/day03.ex
+++ lib/advent_of_code2024/day03.ex
@@ -19,9 +19,30 @@ defmodule AdventOfCode2024.Day03 do
   end
 
   @doc """
-  Solves part 2 of the day's challenge
+  Solves part 2 of the day's challenge by handling do() and don't() instructions.
+  - do() enables future mul instructions
+  - don't() disables future mul instructions
+  - mul instructions start enabled
+  - only the most recent do() or don't() instruction applies
   """
   def part2(input) do
-    0
+    # Find all instructions in order: mul(X,Y), do(), and don't()
+    instructions =
+      Regex.scan(~r/(?:mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))/, input)
+      |> Enum.map(fn
+        ["do()" | _] -> {:do}
+        ["don't()" | _] -> {:dont}
+        [mul, x, y] -> {:mul, String.to_integer(x), String.to_integer(y)}
+      end)
+
+    # Process instructions in order, tracking enabled state
+    {_enabled, sum} =
+      Enum.reduce(instructions, {true, 0}, fn
+        {:do}, {_enabled, sum} -> {true, sum}
+        {:dont}, {_enabled, sum} -> {false, sum}
+        {:mul, x, y}, {enabled, sum} -> {enabled, sum + if(enabled, do: x * y, else: 0)}
+      end)
+
+    sum
   end
 end
--- test/advent_of_code2024/day03_test.exs
+++ test/advent_of_code2024/day03_test.exs
@@ -4,11 +4,12 @@ defmodule AdventOfCode2024.Day03Test do
 
   alias AdventOfCode2024.Day03
 
-  @example_input "x mul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+  @example_input_part1 "x mul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+  @example_input_part2 "x mul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)un do()?mul(8,5))"
 
   describe "part1/1" do
     test "solves the example case" do
-      assert Day03.part1(@example_input) == 161
+      assert Day03.part1(@example_input_part1) == 161
     end
 
     test "handles empty input" do
@@ -33,11 +34,26 @@ defmodule AdventOfCode2024.Day03Test do
 
   describe "part2/1" do
     test "solves the example case" do
-      assert Day03.part2(@example_input) == 0
+      assert Day03.part2(@example_input_part2) == 48
     end
 
     test "handles empty input" do
       assert Day03.part2("") == 0
+    end
+
+    test "handles do() and don't() instructions" do
+      # 2*3 + 6*7
+      assert Day03.part2("mul(2,3)don't()mul(4,5)do()mul(6,7)") == 48
+    end
+
+    test "handles multiple do() and don't() instructions" do
+      # 2*3 + 6*7
+      assert Day03.part2("do()mul(2,3)don't()do()don't()mul(4,5)do()mul(6,7)") == 48
+    end
+
+    test "starts with mul instructions enabled" do
+      # just 2*3
+      assert Day03.part2("mul(2,3)don't()mul(4,5)") == 6
     end
   end
 end
